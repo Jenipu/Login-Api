b1f04bb798076f9bf8c46cac2a89f53310092f06
Jennifer Pulido <jenipu2828@gmail.com>
2024-07-30 19:39:07 -0500

    modellUser

---
+++ node/routes/userRoutes.js
@@ -0,0 +1,10 @@
+
+import express from 'express'
+import { getAllUsers } from '../controllers/UserController.js'
+
+
+const routerUser = express.Router()
+
+routerUser.get('/', getAllUsers)
+
+export default  routerUser
